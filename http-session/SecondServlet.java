4ea72c8fa2d20e14c7da2bd4199041586731ea04
Phatcharadol Thangplub <43667929+boz3r@users.noreply.github.com>
2023-10-29 23:09:46 +0700

    Update SecondServlet.java

--- http-session/SecondServlet.java
+++ http-session/SecondServlet.java
@@ -14,6 +14,7 @@ import java.io.PrintWriter;
 public class SecondServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //session เน้นเก็บทางฝั่ง server พวกข้อมูลของ user บลาๆ (มีความปลอดภัยมากกว่า cookies) จะหายไปก็ต่อเมื่อ user ปิด tab ต่างๆ
         HttpSession session = request.getSession();
         String name = (String) session.getAttribute("username");
 
